2c217b9016000100d1de50e159a4c4c6edd8be54
jacobj10 <jacobj10@bbhcsd.org>
2017-03-21 19:17:27 -0400

    Parses and handles date ranges correctly

--- sicc.py
+++ sicc.py
@@ -1,25 +1,44 @@
 import os
 import random
-from hashlib import sha1
-from random import random
+import datetime
+
 import git
 
+FORMAT_STRING = " 01:01:{0}"
 
-repo_dir = os.path.join(os.getcwd(), 'GitSicco')
+def generate_repo(startday, mask):
+    repo_dir = os.path.join(os.getcwd(), 'GitSicco')
+    new_file = os.path.join(repo_dir, 'sicc')
 
-new_file = os.path.join(repo_dir, 'sicc')
+    r = git.Repo.init(repo_dir)
 
-r = git.Repo.init(repo_dir)
+    f = open(new_file, 'wb')
+    date = startday
+    for i in range(len(mask)):
+        for j in range(mask[i]):
+            date_string = get_date_string(date, j)
+            print(date_string)
+            r.index.add([new_file])
+            r.index.commit("commit{0}.{1}".format(str(i), str(j)))
+            hexsha = r.head.commit.hexsha
+            bash_string = """
+            if [ $GIT_COMMIT = {0} ]
+             then
+                 export GIT_AUTHOR_DATE="{1}"
+                 export GIT_COMMITTER_DATE="{1}"
+             fi""".format(hexsha, date_string)
+            r.git.filter_branch('-f', '--env-filter', bash_string)
+        date += 1
 
-f = open(new_file, 'wb')
-for i in range(5):
-    r.index.add([new_file])
-    r.index.commit("commit{0}".format(str(i)))
-    hexsha = r.head.commit.hexsha
-    bash_string = """
-    if [ $GIT_COMMIT = {0} ]
-     then
-         export GIT_AUTHOR_DATE="Fri Jan 2 21:38:53 2009 -0800"
-         export GIT_COMMITTER_DATE="Sat May 19 01:01:01 2007 -0700"
-     fi""".format(hexsha)
-    r.git.filter_branch('-f', '--env-filter', bash_string)
+def get_date_string(date, int_sec):
+    sec = str(int_sec)
+    str_sec = sec
+    if len(sec) != 2:
+        str_sec = '0' + sec
+    temp = datetime.date.fromordinal(date)
+    iso_array = temp.isoformat().split('-')
+    iso_array[0] = iso_array[0][-2:]
+    to_return = iso_array[1] + '-' +  iso_array[2] + '-' + iso_array[0]
+    return to_return + FORMAT_STRING.format(str_sec)
+
+generate_repo(300000, [1,2,1])
